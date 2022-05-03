da00a14a898548cf550e1f846068b51ebec647d2
Marko Miletic <mtm8@bfh.ch>
2022-05-03 07:28:43 +0200

    progress on securityplan navigation
    progress on securityplan navigation

--- src/containers/SecurityplanContainer.tsx
+++ src/containers/SecurityplanContainer.tsx
@@ -1,311 +1,156 @@
-import { CarePlan, CarePlanIntent, CarePlanStatus, Resource, Reference } from '@i4mi/fhir_r4';
+import {Resource} from '@i4mi/fhir_r4';
+import {StackNavigationProp} from '@react-navigation/stack';
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, ImageBackground} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import { connect } from 'react-redux';
-import SecurityPlanModel from '../model/SecurityPlan';
+import {connect} from 'react-redux';
+import AppButton from '../components/AppButton';
+import EmergencyNumberButton from '../components/EmergencyNumberButton';
+import MainNotification from '../components/MainNotification';
+import LocalesHelper from '../locales';
+import MidataService from '../model/MidataService';
 import UserProfile from '../model/UserProfile';
-import { AppStore } from '../store/reducers';
-import * as midataServiceActions from '../store/midataService/actions';
-import * as userProfileActions from '../store/userProfile/actions';
-import { Button } from 'native-base';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {AppStore} from '../store/reducers';
+import {
+  AppFonts,
+  appStyles,
+  colors,
+  scale,
+  TextSize,
+} from '../styles/App.style';
 
 interface PropsType {
+  navigation: StackNavigationProp<any>;
+  localesHelper: LocalesHelper;
+  midataService: MidataService;
   userProfile: UserProfile;
+  addResource: (r: Resource) => void;
   synchronizeResource: (r: Resource) => void;
-  replaceSecurityPlan: (newPlan: SecurityPlanModel, oldPlan: SecurityPlanModel, u: Reference) => void;
 }
 
 interface State {
-  securityPlan: SecurityPlanModel;
+  bubbleVisible: boolean;
+  listVisible: boolean;
 }
 
-// just for testing, delete before merge
-const TEST_PLAN: CarePlan = {
-  resourceType: 'CarePlan',
-  status: CarePlanStatus.ACTIVE,
-  id: 'sicherheitsplan',
-  contained: [
-      {
-          resourceType: 'CarePlan',
-          id: 'modul1',
-          status: 'active',
-          intent: 'plan',
-          title: 'Meine Motivation zu leben',
-          category: [
-              {
-                  coding: [
-                      {
-                          system: 'http://midata.coop/sero/CODESYSTEMTBD',
-                          code: 'securityplan/motivation'
-                      }
-                  ]
-              }
-          ],
-          description: 'Was gibt mir Kraft? Was hält mich am Leben? Was möchte ich noch erleben?',
-          activity: [
-              {
-                  detail: {
-                      status: 'unknown',
-                      description: 'Fische füttern'
-                  }
-              }
-          ]
-      },
-      {
-          resourceType: 'CarePlan',
-          id: 'modul2',
-          status: 'active',
-          intent: 'plan',
-          title: 'Meine Frühwarnzeichen',
-          category: [
-              {
-                  coding: [
-                      {
-                          system: 'http://midata.coop/sero/CODESYSTEMTBD',
-                          code: 'securityplan/warningSigns'
-                      }
-                  ]
-              }
-          ],
-          description: 'Welche Gedanken, Gefühle oder Verhaltensweisen treten bei mir vor einer suizidalen Krise auf?',
-          activity: [
-              {
-                  detail: {
-                      status: 'unknown',
-                      description: 'Krokodile küssen'
-                  }
-              }
-          ]
-      },
-      {
-          resourceType: 'CarePlan',
-          id: 'modul3',
-          status: 'active',
-          intent: 'plan',
-          title: 'Meine Bewältigungsstrategien',
-          category: [
-              {
-                  coding: [
-                      {
-                          system: 'http://midata.coop/sero/CODESYSTEMTBD',
-                          code: 'securityplan/copingStrategies'
-                      }
-                  ]
-              }
-          ],
-          description: 'Was tue ich selbst, wenn Suizidgedanken stärker werden? Was hilft mir in solchen Augenblicken?',
-          activity: [
-              {
-                  detail: {
-                      status: 'unknown',
-                      description: 'Schafe streicheln'
-                  }
-              }
-          ]
-      },
-      {
-          resourceType: 'CarePlan',
-          id: 'modul4',
-          status: 'active',
-          intent: 'plan',
-          title: 'Meine Ablenkungsstrategien',
-          category: [
-              {
-                  coding: [
-                      {
-                          system: 'http://midata.coop/sero/CODESYSTEMTBD',
-                          code: 'securityplan/distractionStrategies'
-                      }
-                  ]
-              }
-          ],
-          description: 'Welche Aktivität tut mir gut? An welchen Orten und Plätzen oder mit welchen Menschen komme ich auf andere Gedanken?',
-          activity: [
-              {
-                  detail: {
-                      status: 'unknown',
-                      description: 'Hunde hüten'
-                  }
-              }
-          ]
-      },
-      {
-          resourceType: 'CarePlan',
-          id: 'modul5',
-          status: 'active',
-          intent: 'plan',
-          title: 'Meine Glaubenssätze',
-          category: [
-              {
-                  coding: [
-                      {
-                          system: 'http://midata.coop/sero/CODESYSTEMTBD',
-                          code: 'securityplan/personalBeliefs'
-                      }
-                  ]
-              }
-          ],
-          description: 'Was sind meine Glaubenssätze? TEXT TBD',
-          activity: [
-              {
-                  detail: {
-                      status: 'unknown',
-                      description: 'Wale wählen'
-                  }
-              }
-          ]
-      },
-      {
-          resourceType: 'CarePlan',
-          id: 'modul6',
-          status: CarePlanStatus.ACTIVE,
-          intent: 'plan',
-          title: 'Meine professionellen Helfer- und Notfallnummern',
-          category: [
-              {
-                  coding: [
-                      {
-                          system: 'http://midata.coop/sero/CODESYSTEMTBD',
-                          code: 'securityplan/professionalContacts'
-                      }
-                  ]
-              }
-          ],
-          description: 'Welche Fachpersonen oder Organisationen unterstützen mich im Notfall?',
-          activity: [
-              {
-                  detail: {
-                      status: 'unknown',
-                      description: 'Beratung LUPS (0900 85 65 65)'
-                  }
-              },
-              {
-                  detail: {
-                      status: 'unknown',
-                      description: 'Die Dargebotene Hand (143)'
-                  }
-              },
-              {
-                  detail: {
-                      status: 'unknown',
-                      description: 'Polizei (117)'
-                  }
-              }
-          ]
-      }
-  ] as CarePlan[],
-  intent: CarePlanIntent.PLAN,
-  title: 'Neuer Sicherheitsplan (27.4.)',
-  created: new Date().toISOString(),
-  subject: {},
-  author: {},
-  basedOn: [
-      {
-          reference: '#modul1',
-          type: 'CarePlan'
-      },
-      {
-          reference: '#modul2',
-          type: 'CarePlan'
-      },
-      {
-          reference: '#modul4',
-          type: 'CarePlan'
-      },
-      {
-          reference: '#modul3',
-          type: 'CarePlan'
-      },
-      {
-          reference: '#modul5',
-          type: 'CarePlan'
-      },
-      {
-          reference: '#modul6',
-          type: 'CarePlan'
-      }
-  ]
-}
-
-class SecurityplanContainer extends Component<PropsType, State> {
+class Securityplan extends Component<PropsType, State> {
   constructor(props: PropsType) {
     super(props);
 
     this.state = {
-      securityPlan: this.props.userProfile.getCurrentSecurityPlan()
+      bubbleVisible: true,
+      listVisible: false,
     };
-  }
-
-  updateSecurityPlan(_plan: SecurityPlanModel): void {
-    const userReference = this.props.userProfile.getFhirReference();
-    if (userReference) {
-      this.props.synchronizeResource(_plan.getFhirResource(userReference));
-    }
-  }
-
-  test() {
-    const modules = this.state.securityPlan.getSecurityPlanModules();
-    const newOrder = [1,2,3,0,5,4];
-    modules[0].entries.push('Neuer Eintrag vom ' + new Date().toLocaleString());
-    modules.forEach((module, index) => {
-      module.order = newOrder[index];
-    });
-    this.state.securityPlan.setModulesWithOrder(modules);
-    this.updateSecurityPlan(this.state.securityPlan);
-  };
-
-  archivePlan() {
-    const userReference = this.props.userProfile.getFhirReference();
-    if (userReference) {
-      this.props.replaceSecurityPlan(
-        new SecurityPlanModel(TEST_PLAN), 
-        this.state.securityPlan, 
-        userReference
-      );
-    }
   }
 
   render() {
     return (
-      <SafeAreaView edges={['top', 'bottom']}>
-        { this.state.securityPlan 
-          ? <View>
-              <Text>Just for testing, can be trashed</Text>
-              <Text>{ this.state.securityPlan.getTitle() }</Text>
-              { this.state.securityPlan.getSecurityPlanModules().map(module => {
-                return (
-                  <View key={module.type}>
-                    <Text>Modul: {module.title}</Text>
-                    <Text> {module.description}</Text>
-                    <Text>TODO...</Text>
-                  </View>
-                );
-              })}
-            <TouchableOpacity onPress={this.test.bind(this)}><Text>Click for Test</Text></TouchableOpacity>
-
-            <TouchableOpacity onPress={this.archivePlan.bind(this)}><Text>Click for Archive</Text></TouchableOpacity>
+      <SafeAreaView style={styles.container} edges={['top']}>
+        <ImageBackground
+          source={require('../resources/images/backgrounds/mood_bg_lightOrange.png')}
+          resizeMode="cover"
+          style={styles.backgroundImage}>
+          <View style={styles.topView}>
+            <View style={styles.topTextView}>
+              <Text style={styles.topViewText}>
+                {this.props.localesHelper.localeString('securityplan.title')}
+              </Text>
             </View>
-          : <Text>SecurityPlan</Text> 
-        }
+            <View style={styles.emergencyButton}>
+              <EmergencyNumberButton />
+            </View>
+          </View>
+
+          <View style={styles.bottomView}>
+            <MainNotification />
+          </View>
+          <View style={appStyles.buttonContainer}>
+            <AppButton
+              label={this.props.localesHelper.localeString(
+                'securityplan.current',
+              )}
+              icon={
+                '<?xml version="1.0" encoding="utf-8"?> <!-- Generator: Adobe Illustrator 26.2.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  --> <svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 146.7 146.7" style="enable-background:new 0 0 146.7 146.7;" xml:space="preserve"> <style type="text/css"> .st0{fill:#FFFFFF;} .st1{fill:none;stroke:#FFFFFF;stroke-width:2.4;stroke-miterlimit:10;} .st2{fill:none;stroke:#FFFFFF;stroke-width:2.4;stroke-linejoin:round;} </style> <g> <path class="st0" d="M45,89.7c14.3,0,25.9-11.6,25.9-25.9S59.2,38,45,38S19.1,49.6,19.1,63.8l0,0l0,0C19.1,78.1,30.7,89.7,45,89.7" /> <circle class="st1" cx="107.9" cy="88.9" r="17.7"/> <rect x="10.8" y="29.8" class="st2" width="125" height="87.1"/> </g> </svg>'
+              }
+              position="right"
+              color={colors.primary}
+              onPress={() => {
+                this.props.navigation.navigate('SecurityplanCurrent');
+              }}
+            />
+            <AppButton
+              label={this.props.localesHelper.localeString('common.archive')}
+              icon={
+                '<?xml version="1.0" encoding="utf-8"?> <!-- Generator: Adobe Illustrator 26.2.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  --> <svg version="1.1" id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 146.7 146.7" style="enable-background:new 0 0 146.7 146.7;" xml:space="preserve"> <style type="text/css"> .st0{fill:#FFFFFF;} .st1{fill:none;stroke:#FFFFFF;stroke-width:2.4;stroke-miterlimit:10;} .st2{fill:none;stroke:#FFFFFF;stroke-width:2.4;stroke-linejoin:round;} </style> <g> <path class="st0" d="M45,89.7c14.3,0,25.9-11.6,25.9-25.9S59.2,38,45,38S19.1,49.6,19.1,63.8l0,0l0,0C19.1,78.1,30.7,89.7,45,89.7" /> <circle class="st1" cx="107.9" cy="88.9" r="17.7"/> <rect x="10.8" y="29.8" class="st2" width="125" height="87.1"/> </g> </svg>'
+              }
+              position="right"
+              color={colors.gold}
+              onPress={() => {
+                this.props.navigation.navigate('SecurityplanArchive');
+              }}
+              style={{height: scale(88)}}
+            />
+          </View>
+        </ImageBackground>
       </SafeAreaView>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  topTextView: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginLeft: scale(50),
+  },
+  topViewText: {
+    color: colors.white,
+    fontFamily: AppFonts.bold,
+    fontSize: scale(TextSize.big),
+  },
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  topTextView: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginLeft: scale(50),
+  },
+  topViewText: {
+    color: colors.white,
+    fontFamily: AppFonts.bold,
+    fontSize: scale(TextSize.big),
+  },
+  emergencyButton: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+  topView: {
+    backgroundColor: 'rgba(203, 95, 11, 0.5)',
+    flex: 1,
+    flexDirection: 'row',
+  },
+  bottomView: {
+    flex: 0.92,
+    backgroundColor: 'rgba(255, 255, 255, 0.65)',
+  },
+});
+
+// Link store data to component :
 function mapStateToProps(state: AppStore) {
   return {
-      userProfile: state.UserProfileStore
+    localesHelper: state.LocalesHelperStore,
+    midataService: state.MiDataServiceStore,
+    userProfile: state.UserProfileStore,
   };
 }
 
-function mapDispatchToProps(dispatch: Function) {
-  return {
-    replaceSecurityPlan: (n: SecurityPlanModel, o: SecurityPlanModel, u: Reference) => 
-      userProfileActions.replaceSecurityPlan(dispatch, n, o, u),
-    synchronizeResource: (r: Resource) => midataServiceActions.synchronizeResource(dispatch, r)
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SecurityplanContainer);
+export default connect(mapStateToProps, undefined)(Securityplan);
