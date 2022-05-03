adbc91220e7f2bf6700040a172354451517ab486
Marko Miletic <mtm8@bfh.ch>
2022-05-03 10:17:16 +0200

    Securityplan speech bubble
    Securityplan speech bubble without logic

---
+++ src/components/SecurityplanSpeechBubble.tsx
@@ -0,0 +1,212 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, TouchableWithoutFeedback, View, Platform} from 'react-native';
+import {connect} from 'react-redux';
+import LocalesHelper from '../locales';
+import {AppStore} from '../store/reducers';
+import {AppFonts, colors, scale, TextSize, verticalScale} from '../styles/App.style';
+import CancelButton from '../resources/images/common/cancel.svg';
+import PersonIcon from '../resources/images/common/person.svg';
+import SpeechBubble from './SpeechBubble';
+import {StackNavigationProp} from '@react-navigation/stack';
+
+export enum SECURITYPLAN_SPEECH_BUBBLE_MODE {
+  edit = 'EDIT',
+  new = 'NEW'
+}
+
+const MENU_ACTIONS = [
+  {name: 'editSecurityplan', mode: SECURITYPLAN_SPEECH_BUBBLE_MODE.edit},
+  {name: 'newSecurityplan', mode: SECURITYPLAN_SPEECH_BUBBLE_MODE.new}
+];
+
+interface SecurityplanBubbleProps {
+  localesHelper: LocalesHelper;
+  navigation: StackNavigationProp<any>;
+}
+
+interface ContactSpeechBubbleState {}
+
+class ContactSpeechBubble extends Component<SecurityplanBubbleProps, ContactSpeechBubbleState> {
+  constructor(props: SecurityplanBubbleProps) {
+    super(props);
+    this.state = {};
+  }
+
+  renderBubbleTitle(_translateString: string) {
+    return (
+      <View style={styles.titleBar}>
+        <Text style={styles.titlebarText}>{this.props.localesHelper.localeString(_translateString)}</Text>
+        <CancelButton
+          width={scale(35)}
+          height={scale(35)}
+          onPress={() => {}}
+        />
+      </View>
+    );
+  }
+
+  renderMenu() {
+    return (
+      <>
+        {this.renderBubbleTitle('securityplan.bubbleTitle')}
+        <View style={styles.actionList}>
+          {MENU_ACTIONS.map((action) => {
+            return (
+              <TouchableWithoutFeedback
+                onPress={() => {}}
+                key={'menu.' + action.name}>
+                <View
+                  style={styles.actionMenuPoint}
+                  key={'action_' + action.name}>
+                  <View style={styles.actionBubble}></View>
+                  <View style={styles.actionTextWrapper}>
+                    <Text style={styles.actionText}>{this.props.localesHelper.localeString('securityplan.' + action.name)}</Text>
+                  </View>
+                </View>
+              </TouchableWithoutFeedback>
+            );
+          })}
+        </View>
+      </>
+    );
+  }
+
+  renderIcon() {
+    return (
+      <View style={{position: 'absolute', justifyContent: 'center', alignContent: 'center', width: '100%', marginLeft: scale(57.5)}}>
+        <PersonIcon
+          width={80}
+          height={80}
+          style={{position: 'absolute', top: 315, alignSelf: 'center'}}
+        />
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <SpeechBubble
+        bubbleContent={this.renderMenu()}
+        stylingOptions={{
+          general: {
+            position: {
+              top: 100,
+              left: -25
+            },
+            width: scale(337.5)
+          },
+          arrow: {
+            position: {
+              left: verticalScale(140),
+              bottom: 0
+            },
+            size: 30
+          }
+        }}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  titleBar: {
+    paddingVertical: scale(10),
+    paddingLeft: scale(40),
+    paddingRight: scale(5),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  titlebarText: {
+    color: colors.primary,
+    fontSize: scale(TextSize.normal),
+    fontFamily: AppFonts.bold
+  },
+  actionList: {
+    marginBottom: scale(30)
+  },
+  actionMenuPoint: {
+    marginLeft: scale(45),
+    flexDirection: 'row',
+    alignItems: 'flex-end'
+  },
+  actionBubble: {
+    backgroundColor: colors.white,
+    borderWidth: 2,
+    marginRight: scale(TextSize.normal),
+    borderColor: colors.primary,
+    borderRadius: scale(TextSize.big) / 2,
+    height: scale(TextSize.big),
+    width: scale(TextSize.big)
+  },
+  actionText: {
+    fontSize: scale(TextSize.small),
+    fontFamily: AppFonts.regular
+  },
+  actionTextWrapper: {
+    borderBottomWidth: 2,
+    marginTop: scale(TextSize.normal) / 1,
+    borderBottomColor: colors.veryLightGrey,
+    borderBottomStyle: 'solid',
+    marginRight: Platform.OS === 'ios' ? scale(-10) : scale(-8.5),
+    flex: 1
+  },
+  formWrapper: {
+    marginTop: scale(10),
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  formButton: {
+    backgroundColor: colors.primary,
+    alignSelf: 'flex-end',
+    height: 2 * scale(TextSize.verySmall),
+    borderTopLeftRadius: scale(TextSize.verySmall),
+    borderBottomLeftRadius: scale(TextSize.verySmall),
+    marginRight: scale(-10),
+    marginTop: scale(TextSize.verySmall),
+    marginBottom: scale(TextSize.verySmall),
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  formButtonText: {
+    paddingHorizontal: scale(TextSize.verySmall),
+    fontSize: scale(TextSize.verySmall),
+    fontFamily: AppFonts.regular,
+    color: colors.white
+  },
+  formInputs: {
+    marginLeft: scale(20),
+    marginRight: scale(-10)
+  },
+  cameraButton: {
+    height: scale(66),
+    width: scale(66),
+    marginLeft: scale(20),
+    borderRadius: scale(33),
+    backgroundColor: colors.veryLightGrey,
+    justifyContent: 'center',
+    padding: scale(12.5)
+  },
+  listItemInitials: {
+    borderRadius: 2 * scale(TextSize.small),
+    height: 4 * scale(TextSize.small),
+    width: 4 * scale(TextSize.small),
+    backgroundColor: colors.petrol
+  },
+  listItemInitialsText: {
+    fontFamily: AppFonts.regular,
+    fontSize: 1.8 * scale(TextSize.small),
+    alignSelf: 'center',
+    marginTop: 0.9 * scale(TextSize.small),
+    color: colors.white
+  }
+});
+
+// Link store data to component:
+function mapStateToProps(state: AppStore) {
+  return {
+    localesHelper: state.LocalesHelperStore
+  };
+}
+
+export default connect(mapStateToProps, undefined)(ContactSpeechBubble);
